9f8e9641f72efbf2a4c9712be20351dc3f96fb45
Andrew <andrewjamesrose@gmail.com>
2022-04-04 22:03:23 +0100

    enums and unions

---
+++ typescript/001_Types/myScript.js
@@ -0,0 +1,14 @@
+//union types, we may wish to have a flexible type
+// eg something that can accept either a string or a number as an input
+function flexibleAdd(num1, num2) {
+    var result;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        result = num1 + num2;
+    }
+    else {
+        result = num1.toString() + num2.toString();
+    }
+    return result;
+}
+console.log(flexibleAdd(15, 2));
+console.log(flexibleAdd("hello ", "world"));
