12bbb7ce6be0d014960f06f4410f8a85ac6dd250
Andrew <andrewjamesrose@gmail.com>
2022-04-18 17:54:23 +0100

    added interfaces

--- typescript/001_Types/myScript.js
+++ typescript/001_Types/myScript.js
@@ -1,75 +1,81 @@
 "use strict";
-//TS now supports maps (hashmaps)
-// Instantiate a map via:
-let myMap = new Map();
-//  Note that the types of key and value are set here in the definition
-// it is also possible to assign key:value pairs to the map at instatiation
-// eg
-let myOtherMap = new Map([
-    ["key1", "value1"],
-    ["key2", "value2"],
-    ["key3", "value3"]
-]);
-// ### Map Methods ###
-//  set
-//  map.set(key, value) 
-//      adds a new entry in the Map.
-myMap.set("Cat", 12);
-myMap.set("Dog", 99);
-myMap.set("Fish", 61);
-//  get
-//  map.get(key) 
-//      retrieves the value for a given key from the Map.
-//      eg
-console.log(myMap.get("Dog")); //prints 99
-//  has
-//  map.has(key) 
-//      checks if a key is present in the Map. Returns true or false.
-//      eg
-console.log(myMap.has("Dog")); //prints true
-console.log(myMap.has("Dolphin")); //prints false
-//
-//  size
-//  map.size 
-//      returns the count of entries in the Map.
-console.log(myMap.size); //prints 3
-//
-//  delete 
-//  map.delete(key)
-//      deletes a key-value pair using its key. 
-//      If key is found and deleted, it returns true, else returns false.
-console.log(myMap.delete("Dolphin")); //prints false
-console.log(myMap.delete("Dog")); //returns true
-//   
-//  clear
-//  map.clear()
-//      deletes all entries from the Map.
-//add dog back:
-myMap.set("Dog", 99);
-//  ### Map Iteration ###
-//      Map entries iterate in the insertion order
-//  keys 
-//  map.keys()
-//      iterates over map keys
-for (let key of myMap.keys()) {
-    console.log(key); //"Cat", "Fish", "Dog"
+// from: https://www.typescriptlang.org/docs/handbook/classes.html
+// classes: feature for making objects
+// defining a basic class with a constructor and a method:
+class Animal {
+    constructor(theName) {
+        this.name = theName;
+    }
+    move(distanceInMeters = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
 }
-//  values
-//  map.values() 
-//      iterates over map values
-for (let value of myMap.values()) {
-    console.log(value); //12, 61, 99
+// Extending a class:
+// we can extend a base class to a modified version of the class
+// when calling functions of the base class in the extended class we need to use the super keyword
+class Cat extends Animal {
+    constructor(name) {
+        super(name);
+    }
+    move(distanceInMeters = 45) {
+        console.log("Slinking...");
+        super.move(distanceInMeters);
+    }
 }
-//  entries
-//  map.entries()
-//      to iterate over map entries
-for (let entry of myMap.entries()) {
-    console.log(entry[0], entry[1]); //"Cat" 12, "Fish" 61, "Dog" 99
+//instantiating a new instance of the custom Cat class
+let whiskerface = new Cat("Whiskers");
+whiskerface.move(20);
+// ###  Public, private, and protected modifiers ###
+//  Properties and methods are by default public on a class
+class StealthAnimal {
+    constructor(theName) {
+        this.name = theName;
+        this.id = Math.floor(Math.random() * (100));
+    }
 }
-//  map
-//  uses object destructuring to iterate over map entries
-// (pretty much an extension of the above)
-for (let [key, value] of myMap) {
-    console.log(key, value); //"Cat" 12, "Fish" 61, "Dog" 99
+let cameleon = new StealthAnimal("Cammo");
+//this line would fail:
+//  console.log(cameleon.name)
+//However, this would work:
+console.log(whiskerface.name);
+//Extending StealthAnimal would work but it would not be possible for the derived class
+// to access the private memebers of the base class. Instead the "protected" modifier should be used
+class ExtendedStealthAnimal extends StealthAnimal {
+    constructor(name) {
+        super(name);
+    }
+    //This would not work as name is private on the base class
+    // sayname(){
+    //     console.log(`my name is ${this.name}`);
+    // }
+    //This works
+    sayID() {
+        console.log(`I'm a Fox, my ID is ${this.id}`);
+    }
 }
+let speakingFox = new ExtendedStealthAnimal("Fantastic Mr");
+speakingFox.sayID();
+// ### Getters and Setters ###
+//  define using the special get and set keywords
+class Thing {
+    constructor() {
+        this._size = 0;
+    }
+    get size() {
+        return this._size;
+    }
+    set size(value) {
+        let num = Number(value);
+        // Don't allow NaN, Infinity, etc
+        if (!Number.isFinite(num)) {
+            this._size = 0;
+            return;
+        }
+        this._size = num;
+    }
+}
+//note that these are not called with brackets, they are just get / set by calling the parameter directly
+let myThing = new Thing();
+myThing.size = 2;
+console.log(`My thing has a size of: $(myThing.size)`);
 //# sourceMappingURL=myScript.js.map
