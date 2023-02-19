385a9b9c684a8fd66ec7d2282374c77f741b8809
Andrew <andrewjamesrose@gmail.com>
2023-02-19 21:57:26 +0000

    improved the game visualisation component

--- angular/worldle-dx/src/app/components/globe-results-visualiser/globe-results-visualiser.component.ts
+++ angular/worldle-dx/src/app/components/globe-results-visualiser/globe-results-visualiser.component.ts
@@ -3,13 +3,16 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { CountryCode } from 'src/app/models/game-logic';
 import { GameLogicService } from 'src/app/services/game-logic.service';
-import { Group, Renderer } from 'three';
+import { Group, Renderer, Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLOBE_SCALAR } from 'src/app/constants';
 import threeGlobe from 'three-globe';
-import { generateAxes, greatCircleFromTwoPoints, markerAtLatLong, wedgeBetweenTwoPoints } from 'src/app/commonFunctions/threeSphereFunctions';
-import { getCentroidLatLong } from 'src/app/commonFunctions/geographyFunctions';
+import { generateAxes, getVector3FromLatLong, greatCircleFromTwoPoints, markerAtLatLong, wedgeBetweenTwoPoints } from 'src/app/commonFunctions/threeSphereFunctions';
+import { degreesToRadians, getCentroidLatLong } from 'src/app/commonFunctions/geographyFunctions';
 import { IDisplayModeState } from '../globe-controls/globe-controls.component';
+
+//############################ TO DO ###########################################
+// Add handler for when a country doesn't have a 3d map...add a dot
 
 const colourList: number[] = [
   0xBF1B39,   //red
@@ -19,6 +22,20 @@ const colourList: number[] = [
   0x45488C,   //purple
   0x999999    //grey
 ]
+
+
+const ALTITUDE_DEFAULT: number = 0.01
+const ALTITUDE_CORRECT: number = 0.5
+const ALTITUDE_INCORRECT: number = 0.20
+
+const COLOUR_GLOBE_CAP_GREY: string = 'rgba(200, 220, 200, 1)'
+const COLOUR_GLOBE_CAP_CORRECT: string = 'rgba(42, 157, 143, 0.8)'
+const COLOUR_GLOBE_CAP_INCORRECT: string = 'rgba(191, 27, 57, 0.6)'
+
+const COLOUR_GLOBE_SIDE_GREY: string = 'rgba(200, 220, 200, 0.6)'
+const COLOUR_GLOBE_SIDE_CORRECT: string = 'rgba(42, 157, 143, 0.6)'
+const COLOUR_GLOBE_SIDE_INCORRECT: string = 'rgba(191, 27, 57, 0.6)'
+
 
 const testCountries: CountryCode[] = ['FI', 'CL', 'JP', 'CA', 'AU', 'DE']
 
@@ -37,10 +54,12 @@ export class GlobeResultsVisualiserComponent implements OnInit {
   private camera!: THREE.PerspectiveCamera;
   private controls!: OrbitControls;
 
-  geoJSONdata: any[] = []
+  geoJSONdata: any
 
   guessList: CountryCode[] = []
   targetCode!: CountryCode
+
+  DEV_GUESS_LIST = ['DE', 'MX', 'BR', 'JP', 'CL']
 
   visualiserDisplayState: IDisplayModeState = {
     resultsGlobe: true,
@@ -62,6 +81,8 @@ export class GlobeResultsVisualiserComponent implements OnInit {
     this.gameLogicService.getPrevioustGuesses()
       .subscribe((guessesIn: CountryCode[]) => {
         this.guessList = guessesIn
+        this.reRenderGlobe()
+        this.highlightCountry(this.guessList.slice(-1)[0])
       })
 
     this.gameLogicService.getTargetCountry()
@@ -104,6 +125,7 @@ export class GlobeResultsVisualiserComponent implements OnInit {
   lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 
 
+
   globe = new threeGlobe({ animateIn: false })
     .globeImageUrl('/assets/img/lightblue.jpg')
     .pointAltitude('size')
@@ -114,21 +136,8 @@ export class GlobeResultsVisualiserComponent implements OnInit {
       next: data => {
 
         this.geoJSONdata = data
-        console.log("filtered data:")
-        console.log(data.features.filter((feature: { properties: { ISO_A2_EH: string; }; }) => feature.properties.ISO_A2_EH === 'FR'))
 
-        type MyType = {
-          [key: string]: any;
-        }
-
-        this.globe
-          .polygonsData(data.features)
-          .polygonCapColor((feat) => { return this.checkColourCap((<MyType>feat)['properties'].ISO_A2_EH) })
-          .polygonSideColor((feat) => { return this.checkColourSide((<MyType>feat)['properties'].ISO_A2_EH) })
-          .polygonStrokeColor(() => '#111')
-          .polygonAltitude(feat => { return this.checkCountry((<MyType>feat)['properties'].ISO_A2_EH) })
-
-
+        this.reRenderGlobe()
 
         this.createScene()
         console.log(this.rendererContainer)
@@ -137,9 +146,89 @@ export class GlobeResultsVisualiserComponent implements OnInit {
         this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
         this.animate();
 
+        // console.log("THIS IS THE DATA")
+        // console.log(data)
+
+        console.log("filtered data:")
+        console.log(data.features.filter((feature: { properties: { ISO_A2_EH: string; }; }) => feature.properties.ISO_A2_EH === 'FR'))
+
+        // type MyType = {
+        //   [key: string]: any;
+        // }
+
+        // this.globe
+        //   .polygonsData(data.features)
+        //   .polygonCapColor((feat) => { return this.getCountryCapColour((<MyType>feat)['properties'].ISO_A2_EH) })
+        //   .polygonSideColor((feat) => { return this.getCountrySideColour((<MyType>feat)['properties'].ISO_A2_EH) })
+        //   .polygonStrokeColor(() => '#111')
+        //   .polygonAltitude(feat => { return this.getCountryAltitute((<MyType>feat)['properties'].ISO_A2_EH) })
+
+
+
+        // this.createScene()
+        // console.log(this.rendererContainer)
+        // // this.renderer.setSize(window.innerWidth, window.innerHeight);
+        // this.renderer.setSize(600, 600);
+        // this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+        // this.animate();
+
       }
     })
   }
+
+  reRenderGlobe(): void {
+    type TempType = {
+      [key: string]: any;
+    }
+
+    this.globe
+      .polygonsData(this.geoJSONdata.features)
+
+    this.globe
+      .polygonsData(this.geoJSONdata.features)
+      .polygonCapColor((feat) => { return this.getCountryCapColour((<TempType>feat)['properties'].ISO_A2_EH) })
+      .polygonSideColor((feat) => { return this.getCountrySideColour((<TempType>feat)['properties'].ISO_A2_EH) })
+      .polygonStrokeColor(() => '#111')
+      .polygonAltitude(feat => { return this.getCountryAltitute((<TempType>feat)['properties'].ISO_A2_EH) })
+  }
+
+  cameraJumpTest(): void {
+
+    console.log("attempting camera jump")
+    this.highlightCountry("AU")
+  }
+
+  highlightCountry(countryCode: CountryCode): void {
+
+    if (countryCode) {
+        let targetLatLong = getCentroidLatLong(countryCode)
+
+        console.log("lat: " + degreesToRadians(targetLatLong.latitude))
+        console.log("long: " + degreesToRadians(targetLatLong.longitude))
+
+        console.log(targetLatLong)
+
+
+        //set spatial xyz position of camera
+        // note that if orbit control limits are set then they override the camera.position.set
+        // and it pegs out at the limit set in OrbitControl
+
+        targetLatLong = getCentroidLatLong(countryCode)
+
+        let newCoords: Vector3
+        let radius: number = 300
+        newCoords = getVector3FromLatLong(targetLatLong, radius)
+
+        this.camera.position.set(newCoords.x, newCoords.y, newCoords.z)
+    }
+}
+
+
+  deleteTest(): void {
+    this.globe.polygonsData([])
+  }
+
+  
 
 
   animate(): void {
@@ -205,7 +294,7 @@ export class GlobeResultsVisualiserComponent implements OnInit {
 
 
     // Set up centroid points
-    _group = new Group();
+    _group = new Group()
     _group.name = "guessCentroids"
     _group.visible = this.visualiserDisplayState[_group.name as keyof IDisplayModeState]
 
@@ -243,51 +332,50 @@ export class GlobeResultsVisualiserComponent implements OnInit {
     this.controls.enablePan = false
   }
 
-  checkCountry(countryCode: CountryCode): number {
-    let guessList = ['DE', 'MX', 'BR', 'JP', 'CL']
-    let correctAnswer = 'AU'
 
-    let altitude: number = 0.01
+  //possible make a single function and a type with type.alt type.side, type.cap
+  private getCountryAltitute(countryCode: CountryCode): number {
+    // let guessList = this.DEV_GUESS_LIST
+    let guessList = this.guessList
 
-    if (countryCode === correctAnswer) {
-      altitude = 0.5
+    let altitude: number = ALTITUDE_DEFAULT
+
+    if (countryCode === this.targetCode) {
+      altitude = ALTITUDE_CORRECT
     } else if (guessList.includes(countryCode)) {
-      altitude = 0.2
+      altitude = ALTITUDE_INCORRECT
     }
 
     return altitude
   }
 
-  checkColourCap(countryCode: CountryCode): string {
-    let guessList = ['DE', 'MX', 'BR', 'JP', 'CL']
-    let correctAnswer = 'AU'
+  private getCountryCapColour(countryCode: CountryCode,): string {
+    // let guessList = this.DEV_GUESS_LIST
+    let guessList = this.guessList
 
-    // let colour: string = 'rgba(42, 157, 143, 0.8)'  //green
-    let colour: string = 'rgba(200, 220, 200, 1)'  //grey
-
+    let colour: string = COLOUR_GLOBE_CAP_GREY
 
     if (guessList.includes(countryCode)) {
-      colour = 'rgba(191, 27, 57, 0.6)'
+      colour = COLOUR_GLOBE_CAP_INCORRECT
     }
-    if (countryCode === correctAnswer) {
-      colour = 'rgba(42, 157, 143, 0.8)'
+    if (countryCode === this.targetCode) {
+      colour = COLOUR_GLOBE_CAP_CORRECT
     }
 
     return colour
   }
 
-  checkColourSide(countryCode: CountryCode): string {
-    let guessList = ['DE', 'MX', 'BR', 'JP', 'CL']
-    let correctAnswer = 'AU'
+  private getCountrySideColour(countryCode: CountryCode): string {
+    // let guessList = this.DEV_GUESS_LIST
+    let guessList = this.guessList
 
-    let sideColor: string = 'rgba(200, 220, 200, 0.6)'  //grey
-    // let sideColor: string = 'rgba(42, 157, 143, 0.6)'
+    let sideColor: string = COLOUR_GLOBE_SIDE_GREY
 
     if (guessList.includes(countryCode)) {
-      sideColor = 'rgba(191, 27, 57, 0.6)'
+      sideColor = COLOUR_GLOBE_SIDE_INCORRECT
     }
-    if (countryCode === correctAnswer) {
-      sideColor = 'rgba(42, 157, 143, 0.6)'
+    if (countryCode === this.targetCode) {
+      sideColor = COLOUR_GLOBE_SIDE_CORRECT
     }
 
     return sideColor
